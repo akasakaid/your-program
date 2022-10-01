644fe43c0c1c238b62d873ebf71bd96e7486827f
Abhiram Ravikumar <abhi12ravi@gmail.com>
2022-10-01 20:44:59 +0530

    Syntax fixes

--- programs/abhi12ravi.py
+++ programs/abhi12ravi.py
@@ -4,8 +4,6 @@
 
 import requests, json
 
- 
-
 # Enter your API key here
 
 api_key = "Your_API_Key"
@@ -22,10 +20,6 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 city_name = input("Enter city name : ")
 
- 
-
-
-
 # complete url address
 
 complete_url = base_url + "appid=" + api_key + "&q=" + city_name
@@ -40,11 +34,7 @@ response = requests.get(complete_url)
 
  
 
-# json method of response object 
-
-# convert json format data into
-
-# python format data
+# json method of response object
 
 x = response.json()
 
@@ -76,59 +66,12 @@ if x["cod"] != "404":
 
     current_temperature = y["temp"]
 
- 
-
-    # store the value corresponding
-
-    # to the "pressure" key of y
-
-    current_pressure = y["pressure"]
-
- 
-
-    # store the value corresponding
-
-    # to the "humidity" key of y
-
-    current_humidity = y["humidity"]
-
- 
-
-    # store the value of "weather"
-
-    # key in variable z
-
-    z = x["weather"]
-
- 
-
-    # store the value corresponding 
-
-    # to the "description" key at 
-
-    # the 0th index of z
-
-    weather_description = z[0]["description"]
-
- 
 
     # print following values
 
     print(" Temperature (in kelvin unit) = " +
 
-                    str(current_temperature) +
-
-          "\n atmospheric pressure (in hPa unit) = " +
-
-                    str(current_pressure) +
-
-          "\n humidity (in percentage) = " +
-
-                    str(current_humidity) +
-
-          "\n description = " +
-
-                    str(weather_description))
+                    str(current_temperature))
 
  
 
