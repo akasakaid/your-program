edf9a8aca2644d7ab3bab91758a12945f09d3775
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-10-01 21:40:10 +0800

    add a python project

---
+++ programs/AbbeyIT.py
@@ -0,0 +1,14 @@
+#Computing Your Age During the Year 2022
+
+birth_Year = int(input("Enter birth year"))
+age = ""
+current_Year = 2022 
+
+age = current_Year - birth_Year
+
+print("Your age is " + str(age))
+
+if age <= 18:
+    print("You are still a minor.")
+else:
+    print("You are now an adult!")
