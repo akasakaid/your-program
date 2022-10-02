bd58ff9b6aa48beea4bf5251024e2768d0f3c32e
akmalfad <90227586+akmalfad@users.noreply.github.com>
2022-10-02 21:40:33 +0700

    akmalfad.py

---
+++ programs/akmalfad.py
@@ -0,0 +1,14 @@
+# Dasar Python kalender
+
+# modul kalender
+import calendar
+
+yy = 2022  # tahun
+mm = 4 # bulan
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
