a4f2208830a51de9baf9129520d0163f239fc7b7
moreharsh <moreharsh2001@gmail.com>
2022-10-03 12:14:40 +0530

    C++ program added

---
+++ programs/moreharsh.cpp
@@ -0,0 +1,25 @@
+// CPP program to compute sum of digits in
+#include <bits/stdc++.h>
+using namespace std;
+
+class gfg {
+public:
+	int getSum(int n)
+	{
+		int sum = 0;
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+		return sum;
+	}
+};
+
+int main()
+{
+	gfg g;
+	int n = 687;
+
+	cout << g.getSum(n);
+	return 0;
+}
